e03721d2bbfd35a593a4d550333ea43533c1ca98
Daan <daan.wendelen@cegeka.be>
2015-04-20 21:55:16 +0200

    added node

---
+++ node/src/main/java/be/cegeka/configurator/Node.java
@@ -0,0 +1,27 @@
+package be.cegeka.configurator;
+
+import be.cegeka.configurator.message.MessageSenderFactory;
+import be.cegeka.configurator.messageProcessor.MessageProcessor;
+import be.cegeka.configurator.messageProcessor.MessageProcessorFactory;
+import be.cegeka.configurator.serverRegistery.ServerRegistery;
+import be.cegeka.configurator.serverRegistery.ServerRegisteryFactory;
+import be.cegeka.configurator.socket.SocketFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Node {
+    public static void main(String[] args) throws Exception {
+        SocketFactory socketFactory = new SocketFactory();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        MessageSenderFactory messageSenderFactory = new MessageSenderFactory(objectMapper);
+
+        MessageProcessorFactory messageProcessorFactory = new MessageProcessorFactory(socketFactory, objectMapper);
+        ServerRegisteryFactory serverRegisteryFactory = new ServerRegisteryFactory(messageSenderFactory, socketFactory, objectMapper);
+
+        MessageProcessor messageProcessor = messageProcessorFactory.create();
+        messageProcessor.start();
+
+        ServerRegistery serverRegistery = serverRegisteryFactory.create(messageProcessor);
+        serverRegistery.start();
+    }
+}
